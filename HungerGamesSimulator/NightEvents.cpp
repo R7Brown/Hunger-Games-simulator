0e7199c0c4e235791da55a9098499068ca1f9fdb
Tyler <Tyler@TLAPTOP>
2018-07-02 11:47:24 -0500

    Added structure to bloodbath events and some to night events. Also set up correct running of night events in simulation() so when they're written they'll execute like day events

--- HungerGamesSimulator/NightEvents.cpp
+++ HungerGamesSimulator/NightEvents.cpp
@@ -1,1 +1,15 @@
 #include "Header.h"
+
+int nightEventOne()
+{
+	string action = "Tribute1 starts a fire";
+	return 0;
+}
+
+int nightEventTwo()
+{
+	string = "Tribute1 sets up camp for the night";
+	return 0;
+}
+
+vector <func_ptr> nightEvents = {};
